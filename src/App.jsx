f02a8132eaebed00eebafa8a7a946b2b146daaad
jun <znznun@gmail.com>
2024-06-12 10:14:53 +0900

    up btn dev

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import React, { useState } from 'react';
 import { SearchScreen } from './components/search';
 import './App.css';
 
-
+const App = () => {
 const API_KEY = import.meta.env.VITE_API_KEY;
 
-const App = () => {
+const upEvent = ()=> {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+}
+
   return (
     <div>
       <SearchScreen API_KEY={API_KEY}/>
+      <div><img src="img/up.png" alt="up" className='up-btn' onClick={upEvent} /></div>
     </div>
   );
 };
